95b497c1add63b411cb9028f7fee651dbffb81b0
Palak Majethia <palakmajethia@gmail.com>
2026-02-08 01:55:28 +0530

    YOUR MESSAGE

--- app.py
+++ app.py
@@ -4,3 +4,5 @@ st.set_page_config(page_title="ExamPrep AI", layout="centered")
 
 st.title("📘 ExamPrep AI")
 st.write("Upload your syllabus or notes and generate exam questions.")
+
+uploaded_file = st.file_uploader("Upload PDF", type=["pdf"])
